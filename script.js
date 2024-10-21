273539033676fedc93c0afa6ce468a9f5dae0606
Gytus <gytis.stravinskas@stud.kitm.lt>
2024-10-21 13:28:49 +0300

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 "use strict";
 
-const hello = (x) => {
+// const dalinimas = (x, y) => {
 
-    alert(`Hello my dear custumer ${x}`)
+//     let ats = y / x
+//     console.log(`atsakymas yra ${ats}`)
+
+// }
+
+// const x = parseInt(prompt(`Input first number`));
+// const y = parseInt(prompt(`Input first number`));
+
+// dalinimas(x, y);
+
+////////////////////////////////
+
+// const age = (x) => {
+//     if (x <= 17){
+//         console.log(`tu esi nepilnametis`)
+//     }
+//     else{
+//         console.log(`tu esi suauges`)
+//     }
+// }
+
+// const x = parseInt(prompt(`Input first number`));
+
+// age(x);
+
+//////////////////////////////////////////
+
+// const hello = (x) => {
+
+//     alert(`Hello my dear custumer ${x}`)
     
-}
+// }
 
-const x = prompt(`Input your name`);
+// const x = prompt(`Input your name`);
 
-hello(x);
+// hello(x);
